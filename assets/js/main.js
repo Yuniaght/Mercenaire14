2a105f3ff7d412ba2e93385a25fef96e3cec3ddf
Jean-Baptiste de Vulder <devulder.jeanbaptiste@gmail.com>
2025-06-06 18:15:44 +0200

    feat: whole index page
    Added new and hot services carousel.
    Added newsletter section
    Added Footer, footer and header (without banner) will be reused in every pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,14 @@
 
 const $navlink = document.getElementById("navlink")
 const $navmenu = document.getElementById("navmenu")
+let mostViewedIndex = 1;
+let mostNewerIndex = 1
+let t1 = null
+let t2 = null
+
+window.addEventListener("resize", resetnav)
+showViewed(mostViewedIndex);
+showNewer(mostNewerIndex)
 
 
 function navmenu(){
@@ -20,4 +28,37 @@ function resetnav() {
   $navmenu.innerHTML = '<i class="icofont-navigation-menu"></i>'
   }
 }
-window.addEventListener("resize", resetnav)
+
+function plusViewed(n) {
+  showViewed(mostViewedIndex += n);
+}
+
+function plusNewer(n) {
+  showNewer(mostNewerIndex += n);
+}
+
+function showViewed(n) {
+  clearTimeout(t1)
+  let i;
+  let hotproduct = document.getElementsByClassName("hotproduct");
+  if (n > hotproduct.length) {mostViewedIndex = 1}
+  if (n < 1) {mostViewedIndex = hotproduct.length}
+  for (i = 0; i < hotproduct.length; i++) {
+    hotproduct[i].style.display = "none";
+  }
+  hotproduct[mostViewedIndex-1].style.display = "block";
+  t1 = setTimeout(plusViewed,5000,1)
+}
+
+function showNewer(n) {
+  clearTimeout(t2)
+  let i;
+  let newproduct = document.getElementsByClassName("newproduct");
+  if (n > newproduct.length) {mostNewerIndex = 1}
+  if (n < 1) {mostNewerIndex = newproduct.length}
+  for (i = 0; i < newproduct.length; i++) {
+    newproduct[i].style.display = "none";
+  }
+  newproduct[mostNewerIndex-1].style.display = "block";
+  t2 = setTimeout(plusNewer,5000,1)
+}
